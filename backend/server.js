1e16c95ff5f3087ed8b5141e3bc8cb1b762a59bb
Bharat Bhammar <bhartbhammar3336@gmail.com>
2023-03-24 17:20:13 +0530

    Login Functionality Added

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,21 @@
-const connectToMongo = require('./config/db.js');
-const express = require('express');
-const bodyParser = require("body-parser")
-const cors = require("cors");
-connectToMongo();
-const app = express();
-const port = 5000;
-const Courses=require('./routes/CourseRoute.js')
-const Chapters=require('./routes/ChapterRoute.js')
-app.use(bodyParser({extended:true}))
+const connectToMongo = require('./config/db.js')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+connectToMongo()
+const app = express()
+const port = 5000
+const Courses = require('./routes/CourseRoute.js')
+const Chapters = require('./routes/ChapterRoute.js')
+// app.use(bodyParser({extended: true}))
 app.use(cors())
-app.use(express.json());
-
-
-
+app.use(express.json())
 
 app.use('/api/auth' , require('./routes/auth'))
-app.use('/api/courses',Courses)
-app.use('/api/chapters',Chapters)
+app.use('/api/courses', Courses)
+app.use('/api/chapters', Chapters)
 // app.use('/api/notes' , require('./routes/notes'))
 
-app.listen(port , ()=>{
-    console.log(`App listening at http://localhost:${port}`)
+app.listen(port , () => {
+  console.log(`App listening at http://localhost:${port}`)
 })
-
-
-
-
-
